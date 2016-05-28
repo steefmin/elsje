02928ad00cbd951ed6177d8e740e4114f033efa1
steefmin <steefmin@gmail.com>
2016-05-28 16:33:20 +0200

    debug some errors and proper production flag evaluation

--- elsje.js
+++ elsje.js
@@ -1,16 +1,14 @@
 require('./env.js');
 var functions = require('./functions');
+var Botkit = require('botkit');
+var os = require('os');
+
 if (!process.env.TOKEN) {
 	console.log('Error: Specify token in environment');
   process.exit(1);
 }
 
-var Botkit = require('botkit');
-var os = require('os');
-
-process.argv.forEach(function(val, index, array){
-  var debug = (val !== "production");
-});
+var debug = (process.argv[2] !== "production");
 
 var controller = Botkit.slackbot({
 	"json_file_store": process.env.STORAGE_LOCATION,
@@ -88,41 +86,45 @@ controller.hears(['what is my name','who am i','wie ben ik','hoe heet ik','wat i
         if (!err) {
           convo.say('Ik weet jouw naam nog niet!');
           convo.ask('Hoe zal ik je noemen?', function(response, convo) {
-          convo.ask('Je wilt dat ik je  `' + response.text + '` noem?', [{
-            pattern: 'yes',
-            callback: function(response, convo) {
-              convo.next();
+            convo.ask('Je wilt dat ik je  `' + response.text + '` noem?', [
+              {
+                pattern: 'yes',
+                callback: function(response, convo) {
+                  convo.next();
+                }
+              },{
+                pattern: 'no',
+                callback: function(response, convo) {
+                  convo.stop();
+                }
+              },{
+                default: true,
+                callback: function(response, convo) {
+                  convo.repeat();
+                  convo.next();
+                }
+              }
+            ]);
+            convo.next();
+          },{'key': 'nickname'}); // store the results in a field called nickname
+          convo.on('end', function(convo) {
+            if (convo.status == 'completed') {
+              bot.reply(message,'Ok! Dit ga ik even opschrijven...');
+              controller.storage.users.get(message.user,function(err, user) {
+                if (!user) {
+                  user = {
+                    id: message.user,
+                  };
+                }
+                user.name = convo.extractResponse('nickname');
+                controller.storage.users.save(user,function(err, id) {
+                  bot.reply(message,'Prima, vanaf nu noem ik je ' + user.name + '.');
+                });
+              });
+            }else{
+              bot.reply(message, 'Ok, laat maar!');
             }
-          },{
-            pattern: 'no',
-            callback: function(response, convo) {
-              convo.stop();
-            }
-          },{
-            default: true,
-            callback: function(response, convo) {
-              convo.repeat();
-              convo.next();
-            }
-          }]);
-          convo.next();
-        },{'key': 'nickname'}); // store the results in a field called nickname
-        convo.on('end', function(convo) {
-        if (convo.status == 'completed') {
-          bot.reply(message,'Ok! Dit ga ik even opschrijven...');
-          controller.storage.users.get(message.user,function(err, user) {
-            if (!user) {
-              user = {
-                id: message.user,
-              };
-            }
-            user.name = convo.extractResponse('nickname');
-            controller.storage.users.save(user,function(err, id) {
-              bot.reply(message,'Prima, vanaf nu noem ik je ' + user.name + '.');
-            });
           });
-        }else{
-          bot.reply(message, 'Ok, laat maar!');
         }
       });
     }

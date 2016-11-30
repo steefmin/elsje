3b131022053eea269c3dabaf8e5b30a561a79ea2
Steef Min <steefmin@gmail.com>
2016-11-30 13:14:46 +0100

    add taskid to trello

--- functions.js
+++ functions.js
@@ -262,7 +262,7 @@ var postSingleTask = function (bot, taskStructure, message) {
   }]
   postAttachment(bot, attachmentArray, taskStructure.channel.id)
   if (bot.TrelloChannels.indexOf(taskStructure.channel.id) >= 0) {
-    postMessage(bot, '/trello add <@' + taskStructure.responsible.id + '> ' + taskstructure.task, taskStructure.channel.id)
+    postMessage(bot, '/trello add <@' + taskStructure.responsible.id + '> ' + taskStructure.taskid + ': ' + taskStructure.task, taskStructure.channel.id)
     postMessage(bot, '/trello set-due ' + deadline.toUTCString().substr(5, 11), taskStructure.channel.id)
   }
 }

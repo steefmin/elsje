14a429157f425f96e8ce4620bf5a6b65da1d2777
Steef Min <steefmin@gmail.com>
2016-12-08 08:43:20 +0100

    Update trellosetup.js

--- trellosetup.js
+++ trellosetup.js
@@ -1,29 +1,29 @@
 var Trello = require('node-trello')
 var functions = require('./functions')
 
-var askTrelloBoard = function (response, convo) {
+var askBoard = function (response, convo) {
   convo.trello = new Trello(process.env.TRELLO_KEY, convo.trelloToken)
   convo.trello.get('/1/members/me/boards/open', function (err, data) {
     if (err) {
       throw err
     }
     // bouw array van boards en boardId's
-    //var array1 = {
-    //  name: boardname1
-    //  text: boardname1
-    //  value: boardId1
-    //  type: 'button'
-    //}  ... etc...
+      var array1 = [{
+    //    name: boardname1
+    //    text: boardname1
+    //    value: boardId1
+    //    type: 'button'
+      }] // ... etc...
     convo.ask({
       attachments: [
         {
-          title: 'Welk board?', 
+          title: 'Welk board?',
           callback_id: 1,
           attachment_type: 'default',
-          actions: array1 //eerdere array
+          actions: array1 // eerdere array
         }
       ]
-    }, function (response, convo){
+    }, function (response, convo) {
       convo.say('Ok')
       askSlackChannel(response, convo)
       convo.next()
@@ -44,7 +44,7 @@ var askSlackChannel = function (response, convo) {
 
 var linkBoardToChannel = function (response, convo) {
   convo.on('end', function (convo) {
-    //link die shit en store 
+    // link die shit en store
   })
 }
 

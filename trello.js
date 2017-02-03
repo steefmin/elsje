d742ce413aaa63a37b95ff42c579f613316586bc
Steef Min <steefmin@gmail.com>
2017-02-03 15:14:27 +0100

    check and store usertoken

--- trello.js
+++ trello.js
@@ -2,6 +2,37 @@ var Trello = require('node-trello')
 
 var trello = new Trello(process.env.TRELLO_KEY, process.env.TRELLO_TOKEN)
 
+var checkUserToken = function (bot, controller, userId, callback) {
+  controller.storage.users.get(userId, function (err, userinfo) {
+    if (!err) {
+      if (userinfo.trellotoken) {
+        callback(trellotoken)
+      } else {
+        bot.startPrivateConversation({user: userId}, function (err, convo) {
+          convo.ask('Open de volgende link en geef mij de code terug: https://trello.com/1/connect?key=' + process.env.TRELLO_KEY + '&name=Elsje&response_type=token. Als je dat niet wilt, zeg \"Nee\"', function (response, convo) {
+            if (response.text !== 'Nee' || response.text !== 'nee') {
+              storeToken(bot, controller, userId, response.text, callback)
+            }
+          })
+        })
+      }
+    }
+  })
+}
+
+var storeToken = function (bot, controller, userId, token, callback) {
+    controller.storage.users.get({user: userId}, function (err, userdata) {
+      if (!err){
+      userdata.trellotoken = token
+        controller.storage.users.save(userdata, function (err, response) {
+          if (!err){
+            callback(userdata.trellotoken)
+          }
+        })
+      }
+    })
+}
+                               
 var newTrelloCard = function (struct) {
   var taskStructure = struct.data
   var cardname = taskStructure.id + ': ' + taskStructure.task
@@ -19,5 +50,6 @@ var newTrelloCard = function (struct) {
 }
 
 module.exports = {
+  checkUserToken: checkToken,
   newTrelloCard: newTrelloCard
 }

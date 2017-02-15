70062cc40f1a69b84c87f4d0f0609f380eaf740e
Steef Min <steefmin@gmail.com>
2017-02-15 08:23:00 +0100

    always at least 1 shade

--- elsje.js
+++ elsje.js
@@ -699,17 +699,17 @@ controller.hears(['leaderboard'], 'mention,direct_mention,direct_message', funct
       attachment.sort(function (a, b) {
         return b.score - a.score
       })
-      var lowestScore = attachment[attachment.length - 1].score
+      var lowScore = attachment[attachment.length - 1].score
       var options = {
         'colormap': 'jet',
-        'nshades': attachment[0].score - lowestScore,
+        'nshades': attachment[0].score - lowScore || 1,
         'format': 'hex',
         'alpha': 1
       }
       var cg = Colormap(options).reverse()
       console.log(cg)
       for (var i = 0; i < attachment.length; i++) {
-        attachment[i].color = cg[attachment[i].score - lowestScore]
+        attachment[i].color = cg[attachment[i].score - lowScore]
       }
       functions.postAttachment(bot, attachment, message.channel)
     }

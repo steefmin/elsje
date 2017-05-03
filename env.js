db16f9dc91a1802f90eb202f7fbe72f6636c1530
stijnholzhauer <stijnholzhauer+gitkraken@gmail.com>
2017-05-03 13:20:10 +0200

    gitignore now working

--- env.js
+++
@@ -1,8 +0,0 @@
-process.env['TOKEN'] = ''
-process.env['STORAGE_LOCATION'] = ''
-
-process.env['WEERWOLVEN_CHANNEL'] = ''
-process.env['WEERWOLVEN_BOT_ID'] = ''
-
-process.env['RESTART_MESSAGE_CHANNEL'] = ''
-

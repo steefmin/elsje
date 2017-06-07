c051c6c3bc6e42719552edc38c389f95af3a3298
Steef Min <steefmin@gmail.com>
2017-06-07 11:28:38 +0200

    reworks parsedb to only parse new tasks
    most completed tasks are useless, so throwing them away. Also now gets backup file i.s.o. the real db.

--- parsedb.js
+++ parsedb.js
@@ -2,34 +2,29 @@ require('./env.js')
 var fs = require('fs')
 var api = require('./svlo-api')
 
-const teamid = ''
+const teamdb = '/teams/db.bak'
 
-var db = fs.readFileSync(process.env.STORAGE_LOCATION + teamid)
+var db = fs.readFileSync(process.env.STORAGE_LOCATION + teamdb)
 
 var json = JSON.parse(db)
 
 json.tasks.forEach(function (oldtask, index, array) {
-  var deadline = new Date(oldtask.deadline)
-  var taskStructure = {
-    'task': oldtask.task,
-    'channel': oldtask.channel.id,
-    'userid': oldtask.user.id,
-    'responsibleid': oldtask.responsible.id,
-    'deadline': deadline.toISOString().substr(0, 10)
-  }
-  console.log('transfering task ' + index + ' out of ' + array.length)
-  api.addTask(taskStructure, function (err, newtask) {
-    if (!err) {
-      if (oldtask.status === 'done') {
-        console.log('also marking the task done')
-        api.completeTask(newtask.taskid, function (err) {
-          if (!err) {
-            console.log('succes')
-          }
-        })
+  if (oldtask.status === 'new') {
+    var deadline = new Date(oldtask.deadline)
+    var taskStructure = {
+      'task': oldtask.task,
+      'channel': oldtask.channel.id,
+      'userid': oldtask.user.id,
+      'responsibleid': oldtask.responsible.id,
+      'deadline': deadline.toISOString().substr(0, 10)
+    }
+    console.log('transfering task ' + index + ' out of ' + array.length)
+    api.addTask(taskStructure, function (err, newtask) {
+      if (err) {
+        console.log(err)
       } else {
         console.log('succes')
       }
-    }
-  })
+    })
+  }
 })
